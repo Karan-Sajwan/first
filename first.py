00616cbdf6573f5bc66adc84b6c0f78471a2d011
Karan-Sajwan <sajwankaran04@gmail.com>
2025-02-08 15:50:58 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,41 @@
+import time
+int a = 1
+int b = 2
+print("integer:",a)
+print("integer:",b)
+Float=1.1
+print("Float:",Float)
+Boolx = True
+Booly = False   
+print("Bool pehla:",Boolx)
+print("Bool dusra:", Booly)
+list1=[1,2,3,4,5]
+list2=[1.1,2.2,3.3,4.4,5.5]
+print("list1=",list1)
+
+####################
+mytuple=("Rohit",22,1.1,"a",True,1)
+mytuplenumber=(1,2,3,4,5)
+
+myset1={1,2,3,4,5,1,1,1,1,1,1,1,1}
+myset2={1.1,2.2,3.3,4.4,5.5}
+
+###
+
+print("_________________________")
+print("mytupple=", mytupple)
+print("_________________________")
+print("myset1=", myset1)
+#
+daysset1={"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"}
+
+daysset2={"Monday","Monday","Monday","Monday","Monday","Monday","Monday","Monday",}
+print("daysset1=", daysset1)
+print("daysset2=", daysset2)
+print("_________________________")
+dict1={"name":"Rohit","age":22,"height": 1.1,"grade":"A","is_student":True}
+dict2={1:"a",2:"b",3:"c",4:"d",5:"e"}
+
+print("dict1=", dict1)
+print("dict2=", dict2)
+print("_________________________")
